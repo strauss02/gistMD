293ebfb3a1eb4cbd85db617295811e3418688cf5
Ido Strauss <strauss02@hotmail.co.il>
2022-04-14 22:56:34 +0300

    Begin implementing react-query

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,22 +3,36 @@ import React from 'react'
 import PatientList from './Components/PatientList'
 import SideNavigation from './Components/SideNavigation'
 
-import { useQuery } from 'react-query'
+import { useQuery, QueryClientProvider, QueryClient } from 'react-query'
 import { getPatients, Patient } from './lib/api'
 
-function App() {
+const queryClient = new QueryClient()
+
+function PatientApp() {
+  const { data: patients } = useQuery<Patient[]>(['patients'], getPatients, {
+    initialData: [],
+  })
+
   return (
     <Container>
       <Grid container>
         <Grid item xs={0} md={2}>
           <SideNavigation />
         </Grid>
-
+        {JSON.stringify(patients)}
         <Grid item xs={12} md={10}>
           <PatientList />
         </Grid>
       </Grid>
     </Container>
+  )
+}
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <PatientApp />
+    </QueryClientProvider>
   )
 }
 
